e021fab01ab791ada1fde932a5c101b500c92e33
papers <paperslove@users.noreply.github.com>
2018-01-16 13:25:33 +0200

    Update StreamElementsController.js
    works now, still using stupid absolute selectors

--- code/js/controllers/StreamElementsController.js
+++ code/js/controllers/StreamElementsController.js
@@ -5,9 +5,9 @@
 
   new BaseController({
     siteName: "StreamElements",
-    playPause: "#dashboard > div > div.songrequest-wrap.layout-row.layout-align-center-start.layout-sm-column.layout-xs-column.ng-scope > md-card.layout-column.layout-align-center-center.element-100pw.songrequest-current-song.flex._md > md-content > div.controls.layout-row.layout-align-start-center.element-100pw > div.layout-row.layout-align-center-center.actions > button:nth-child(1)",
-    playNext: "#dashboard > div > div.songrequest-wrap.layout-row.layout-align-center-start.layout-sm-column.layout-xs-column.ng-scope > md-card.layout-column.layout-align-center-center.element-100pw.songrequest-current-song.flex._md > md-content > div.controls.layout-row.layout-align-start-center.element-100pw > div.layout-row.layout-align-center-center.actions > button:nth-child(2)",
-    song: "#dashboard > div > div.songrequest-wrap.layout-row.layout-align-center-start.layout-sm-column.layout-xs-column.ng-scope > md-card.layout-column.layout-align-center-center.element-100pw.songrequest-current-song.flex._md > md-content > div.info.element-100pw > h1",
+    playPause: "#dashboard > div > div.songrequest-wrap.layout-row.layout-align-center-start.layout-sm-column.layout-xs-column > md-card > md-content > div.controls.layout-row.layout-align-start-center.element-100pw > div.layout-row.layout-align-center-center.actions > button:nth-child(1)",
+    playNext: "#dashboard > div > div.songrequest-wrap.layout-row.layout-align-center-start.layout-sm-column.layout-xs-column > md-card > md-content > div.controls.layout-row.layout-align-start-center.element-100pw > div.layout-row.layout-align-center-center.actions > button:nth-child(2)",
+    song: "#dashboard > div > div.songrequest-wrap.layout-row.layout-align-center-start.layout-sm-column.layout-xs-column > md-card > md-content > div.info.element-100pw > div > h1",
     iframe: ".video"
   });
 })();
